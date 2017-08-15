2fca03f4287b31ec61665baa8bdc264259f075fb
Will <pan123033@qq.com>
2017-08-15 14:03:56 +0800

    test

--- src/app/pages/reserve/reserve.component.ts
+++ src/app/pages/reserve/reserve.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 import { NavTabComponent } from './../../common/nav-tab/nav-tab.component';
 
 @Component({
@@ -6,11 +6,27 @@ import { NavTabComponent } from './../../common/nav-tab/nav-tab.component';
   templateUrl: './reserve.component.html',
   styleUrls: ['./reserve.component.css']
 })
-export class ReserveComponent implements OnInit {
+export class ReserveComponent implements OnChanges, OnInit {
+  @Input() count: number;
 
-  constructor() { }
+  constructor() {
+    this.count = 0;
+   }
 
   ngOnInit() {
+  }
+
+  /* 它会获得一个 SimpleChanges 对象，包含绑定属性的新值和旧值，它主要用于监测组件输入属性的变化 */
+  ngOnChanges(changes: SimpleChanges) {
+    console.dir(changes['count']);
+  }
+
+  addOne(): void {
+    this.count++;
+  }
+
+  MuseOne(): void {
+    this.count--;
   }
 
 }
