76adcfd897517d9c5616702a32c49f0f0f10c15e
Will <pan123033@qq.com>
2017-08-15 16:54:18 +0800

    Revert "test"
    This reverts commit 2fca03f4287b31ec61665baa8bdc264259f075fb.

--- src/app/pages/reserve/reserve.component.ts
+++ src/app/pages/reserve/reserve.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavTabComponent } from './../../common/nav-tab/nav-tab.component';
 
 @Component({
@@ -6,27 +6,11 @@ import { NavTabComponent } from './../../common/nav-tab/nav-tab.component';
   templateUrl: './reserve.component.html',
   styleUrls: ['./reserve.component.css']
 })
-export class ReserveComponent implements OnChanges, OnInit {
-  @Input() count: number;
+export class ReserveComponent implements OnInit {
 
-  constructor() {
-    this.count = 0;
-   }
+  constructor() { }
 
   ngOnInit() {
-  }
-
-  /* 它会获得一个 SimpleChanges 对象，包含绑定属性的新值和旧值，它主要用于监测组件输入属性的变化 */
-  ngOnChanges(changes: SimpleChanges) {
-    console.dir(changes['count']);
-  }
-
-  addOne(): void {
-    this.count++;
-  }
-
-  MuseOne(): void {
-    this.count--;
   }
 
 }
