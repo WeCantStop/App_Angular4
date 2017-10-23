c447cc324678b0f13d6c269d2270faa52f7e06b3
Will <pan123033@qq.com>
2017-10-23 16:53:42 +0800

    Revert "douban index"
    This reverts commit a52c7a072782e489cf9709590a898c26cde10a2b.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { TranslateService } from 'ng2-translate';
+import { SkinType } from 'ngx-weui';
+import { ActionSheetService, ActionSheetConfig, ActionSheetComponent } from 'ngx-weui/actionsheet';
+
+import { Mask } from '../../animations/mask';
 import { FadeIn } from '../../animations/fadeIn.animation';
+import { MaskComponent } from '../../components/mask/mask.component';
 import { ApiService } from '../../services/doubanAPI';
 
 @Component({
@@ -7,21 +13,56 @@ import { ApiService } from '../../services/doubanAPI';
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.css'],
     // 注册动画
-    animations: [ FadeIn ]
+    animations: [ Mask, FadeIn ]
 })
 export class HomeComponent implements OnInit {
+    @ViewChild('ios') iosAS: ActionSheetComponent;
+    @ViewChild('mask') mask: MaskComponent;
+    menus: any[] = [
+        { text: 'item1', key: '1' },
+        { text: 'item2', key: '2' },
+        { text: 'item3', key: '3' }
+    ];
+    config: ActionSheetConfig = <ActionSheetConfig>{
+        title: '请选择其中一个item'
+    };
 
-    public articles;
+    public show = 'show';
     constructor(
+        public translate: TranslateService,
+        public actionSheet: ActionSheetService,
         public apiService: ApiService
     ) { }
 
+    persons = ['one', 'two', 'three', 'four'];
+
     ngOnInit() {
         this.apiService.initData({}).subscribe(res => {
-            this.articles = res.recommend_feeds;       
+            console.log(res);
         });
+    }
 
+    btnClick() {
+        this.translate.currentLang === 'zh_CN' ? this.translate.use('en_US') : this.translate.use('zh_CN');
+    }
 
+    clickL() {
+        console.log('click left');
+    }
+
+    onShow(type: SkinType) {
+        this.config.skin = type;
+        this.config = Object.assign({}, this.config);
+        setTimeout(() => {
+            (<ActionSheetComponent>this[`${type}AS`]).show().subscribe((res: any) => {
+                console.log('type', res);
+            });
+        }, 10);
+    }
+
+    animate(isNormal: boolean = false) {
+        this.mask.show();
+        this.mask.backdrop = true;
     }
 
 }
